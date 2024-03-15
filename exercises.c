232e43f710f74d14446fc73c6737db7834042da2
Your Name <you@example.com>
2024-03-15 22:27:43 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -98,8 +98,9 @@ int checkSorted(int arr[], int size) {
     while (i < size - 2) {
       if (arr[i] < arr[i + 1]) {
         i++;
-        if (arr[i + 1] == arr[size - 1]) {
-          return 1;
+        if (i == size - 2) {
+          if (arr[i] < arr[i + 1]) {
+            return 1;
         }
         return 0;
       }
@@ -107,11 +108,12 @@ int checkSorted(int arr[], int size) {
       
       else if (arr[i] > arr[i + 1]) {
         i++;
-        if (arr[i + 1] == arr[size - 1]) {
-          return -1;
+        if (i == size - 2) {
+            if (arr[i] > arr[i + 1]) {
+              return 1;
+          }
+          return 0;
         }
-        return 0;
-      }
     }
   return 0;
   }
